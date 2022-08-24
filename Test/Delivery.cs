73a4b6c6a76f3f22d7d631fe741a93c33e36c985
DanT0x <bax9999999@mail.ru>
2022-08-24 15:24:19 +0300

    Test
    test

---
+++ Test/Delivery.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class Delivery
+    {
+        public int id { get; set; }
+        public string Name { get; set; } 
+        public int Date { get; set; }
+    }
+}
+
+
+
+
+
